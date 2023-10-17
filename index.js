00c5913a677ed48bfc8f05fa804a63aa3b059d10
[Mayra] <[mayrayaz03@gmail.com]>
2023-10-17 19:11:15 -0300

    Express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-console.log("HI!")
-console.log("HI!")
-console.log("HI!")
-console.log("HI!")
-console.log("HI!")
-console.log("HI!")
+import express from "express"
+/* console.log("🚀 ~ file: index.js:2 ~ express:", express) */
+
+const app = express()
+
+
+
+app.listen(8080, ()=>{
+
+console.log("🚀 ~ file: index.js:8 ~ app.listen ~ http://localhost:8080")
+})
