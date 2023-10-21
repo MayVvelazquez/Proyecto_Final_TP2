d2dfeafd8f16be2875bd2d2f2db11bf894c97ca5
Francisco Ramponi <ramponifran@gmail.com>
2023-10-20 23:37:49 -0300

    Connection establecida

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express"
+import connection from "./connection/connection.js"
 /* console.log("🚀 ~ file: index.js:2 ~ express:", express) */
+
+
 
 const app = express()
 
-
-
 app.listen(8080, ()=>{
 
-console.log("🚀 ~ file: index.js:8 ~ app.listen ~ http://localhost:8080")
+console.log("http://localhost:8080")
 })
