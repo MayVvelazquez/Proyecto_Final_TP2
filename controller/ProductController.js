9261c36486431714c71ca73d156f760d46e4bd66
Francisco Ramponi <ramponifran@gmail.com>
2023-11-21 13:08:37 -0300

    Acomodar Product Controller y validateProduct

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,4 +1,7 @@
 import { Product, ProductTags, Tag, User } from "../Models/index.js";   //Desglozamos esto porque los productos van a necesitar estos
+import validateResult from "express-validator";
+
+import validateProducto from "../mildleares/validateProduct.js";
 
 
 class ProductController {
@@ -9,18 +12,6 @@ class ProductController {
         try {
             const products = await Product.findAll({
                 attributes: ["id", "name", "description", "stock", "price"], 
-                include: [
-                    {
-                        model: Tag,
-                        attributes: ["name"], 
-                        through: { attributes: ["productId", "tagId"] }, // Atributos de ProductTags
-                    },
-                    {
-                        model: User,
-                        as: "vendorId", 
-                        attributes: ["id", "name", "email"], 
-                    },
-                ],
             });
             res.status(200).send({ success: true, message: "Todos los productos", data: products })
         } catch (error) {
@@ -34,63 +25,51 @@ class ProductController {
             const { id } = req.params;
             const product = await Product.findOne({
                 where: { id: id },
-                include: {
-                    model: User,
-                    attributes: ['id', 'name', 'email'], 
-                },
             });
 
-            if (!product.dataValues) {
-                return res.status(404).send({ success: false, message: 'Producto no encontrado' });
+            if (!product) {
+               throw new Error()
             }
     
-            res.status(200).send({ success: true, message: 'Producto encontrado', userId });
+            res.status(200).send({ success: true, message: 'Producto encontrado', data: product });
         } catch (error) {
-            res.status(500).send({ success: false, message: 'Error al obtener el producto', error: error.message });
+            res.status(500).send({ success: false, message: 'Producto/id no encontrados'});
         }
     };
 
-    //Crear producto podriamos vincular un producto por el id de un user creado
+
     createProduct = async (req, res) => {
         try {
+            // const errors = await validateProducto(req);
+        
+            // if (!errors.isEmpty()) {
+            //   const errorMessages = errors.array().map(error => error.msg);
+            //   return res.status(400).send({ success: false, message: errorMessages });
+            // }
+        
             const { name, description, stock, price } = req.body;
-            console.log("Datos recibidos:", { name, description, stock, price });
-            if (!name || !description || stock === undefined || price === undefined) {
-                return res.status(400).send({ success: false, message: 'Faltan datos para crear el producto' });
-            }
-           /*  if (!name) {
-                return res.status(400).send({ success: false, message: 'Faltan nombre' });
-            }if (!description) {
-                return res.status(400).send({ success: false, message: 'Faltan descripcion' });
-            } if (!stock) {
-                return res.status(400).send({ success: false, message: 'Faltan stock' });
-            }else{
-                return res.status(400).send({ success: false, message: 'Faltan precio' });
-            }; */
-
-            const product = await Product.create({ name, description, stock, price})
-            console.log(product)
-            res.status(201).send({ success: true, message: 'Producto creado', data: product });
-        } catch (error) {
-            console.error(error);
-            res.status(500).send({ success: false, message: 'Error al crear el producto' });
-        }
-    };
+            const product = await Product.create({ name, description, stock, price });
+            console.log(product);
+            res.status(200).send({ success: true, message: "Producto creado" });
+        
+          } catch (error) {
+            console.log(error);
+            res.status(500).send({ success: false, message: "Error al crear el producto" });
+          }
+        };
+            
 
     //Actualiza producto
     updateProduct = async (req, res) => {
         try {
             const { id } = req.params;
             const { name, description, stock, price} = req.body;
-            const product = await Product.findByPk(id); //Busca si existe o no el producto por el id
+            const product = await Product.findByPk(id); 
             if (!product) {
                 return res.status(404).send({ success: false, message: 'Producto no encontrado' });
             }
-            // Actualizar los campos del producto
+            
             await product.update({ name, description, stock, price });
-
-            // Eliminar las relaciones actuales entre el producto y las etiquetas
-            /* await ProductTags.destroy({ where: { productId: id } }); DESCOMENTAR POR SI LLEGAMOS HACER LA VINCULACION DE LOS TAGS*/
 
             res.status(200).send({ success: true, message: 'Producto actualizado', data: product });
         } catch (error) {
@@ -104,20 +83,14 @@ class ProductController {
             const { id } = req.params;
 
             const product = await Product.findByPk(id);
-
-            if (!product) throw new Error('Producto no encontrado')/* {
-                return res.status(404).send({ success: false, message: 'Producto no encontrado' });
-            } */
-
-            // Eliminar relacion con los tags antes de eliminar el producto
-            /* await ProductTags.destroy({ where: { productId: id } }); */
-
-            await product.destroy();
-
-            res.status(200).send({ success: true, message: 'Producto eliminado' });
+          
+                await product.destroy();
+              
+                res.status(200).send({ success: true, message: 'Producto eliminado' });
+                    
         } catch (error) {
             console.error(error);
-            res.status(500).send({ success: false, message: 'Error al eliminar el producto' });
+            res.status(500).send({ success: false, message: 'Id no encontrado'});
         }
     };
 
---
+++ mildleares/validateProduct.js
@@ -0,0 +1,24 @@
+import { body, validationResult} from "express-validator";
+
+export const validateProducto = [
+  body('name').notEmpty().withMessage('El nombre no puede estar vacío'),
+  body('description').notEmpty().withMessage('La descripción no puede estar vacía'),
+  body('stock')
+    .notEmpty().withMessage('El stock no puede estar vacío')
+    .isNumeric().withMessage('El stock debe ser un número')
+    .isInt({ min: 1 }).withMessage('El stock debe ser un número entero positivo'),
+  body('price')
+    .notEmpty().withMessage('El precio no puede estar vacío')
+    .isNumeric().withMessage('El precio debe ser un número'),
+  
+  
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+export default validateProducto;
